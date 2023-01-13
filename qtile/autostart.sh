78f45a74e79673056946a0fc750b8d9ce2645a90
Aarrayy <lp610mh@gmail.com>
2023-01-13 11:34:45 -0300

    Add initial dotfiles

---
+++ qtile/autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+feh --bg-fill /home/array/.config/qtile/Aesthetic2.png &
+picom --config $HOME/.config/qtile/scripts/picom.conf & 
+#pavucontrol & 
+#kitty &
+#cat /home/unreal/.cache/wal/sequences &
